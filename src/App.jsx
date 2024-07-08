23540949328f20bd23a9410e0d36c60dacef78e4
Ghostrealm7 <shahjavedgm@gmail.com>
2024-07-08 18:58:39 +0600

    updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,54 @@ function App() {
   const [homePage, setHomePage] = useState(true);
   const [darkMode, setDarkMode] = useState(false);
   const [quizData, setQuizData] = useState([]);
-  // const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [correctAnswerCount, setCorrectAnswerCount] = useState(0);
   const [gameOver, setGameOver] = useState(false);
 
   function checkAnswers() {
+    let count = 0;
     for (let i = 0; i < quizData.length; i++) {
       if (quizData[i].correct_answer === quizData[i].selected_answer) {
         console.log(`Your answer ${quizData[i].selected_answer} is correct`);
+        count++;
       } else {
         console.log(
           `Your answer ${quizData[i].selected_answer} is incorrect, the correct answer is ${quizData[i].correct_answer}`,
         );
       }
     }
+    setCorrectAnswerCount(count);
+    console.log(`You got ${count}/5 answers right!`);
+    setGameOver(true);
   }
 
-  function getNewQuiz() {}
+  async function getNewQuiz() {
+    let response = await axios.get(
+      "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple",
+    );
+    let quizQuestions = await response.data.results;
+    // Save correct_answer in the incorrect answer array, then shuffle the incorrect answer array and save in NewArray.
+    const newArray = quizQuestions.map((quizQuestion) => {
+      const newAnswers = [
+        ...quizQuestion.incorrect_answers,
+        quizQuestion.correct_answer,
+      ];
+      const shuffledAnswers = shuffle(newAnswers);
+      return {
+        id: nanoid(),
+        ...quizQuestion,
+        incorrect_answers: shuffledAnswers,
+        selected_answer: "",
+      };
+    });
+    setQuizData(newArray);
+  }
+
+  function newGame() {
+    getNewQuiz();
+    setGameOver((prevState) => !prevState);
+    setCorrectAnswerCount(0);
+  }
+
   // put axios call in a function so you can call it ousite useEffect
 
   // Shuffle algorithm - Fisher-Yates shuffle
@@ -40,15 +72,12 @@ function App() {
   //function to update quizData array with selected_answer key
   function insertSelectedAnswer(event, id) {
     const { value } = event.target;
-    console.log(value);
-    console.log("insert function fired");
     const updatedQuiz = quizData.map((quizQuestion) => {
       return id === quizQuestion.id
         ? { ...quizQuestion, selected_answer: value }
         : quizQuestion;
     });
     setQuizData((prevQuizData) => (prevQuizData = updatedQuiz));
-    console.log(quizData);
   }
 
   function changePage() {
@@ -58,28 +87,8 @@ function App() {
   //tast for tomorrow, put setquizdata in a function, run a for loop to populate the new array where the correct answer is randomly put with fake answers and save it in another array
   useEffect(() => {
     console.log("effect ran");
-    async function apiData() {
-      let response = await axios.get(
-        "https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple",
-      );
-      let quizQuestions = await response.data.results;
-      // Save correct_answer in the incorrect answer array, then shuffle the incorrect answer array and save in NewArray.
-      const newArray = quizQuestions.map((quizQuestion) => {
-        const newAnswers = [
-          ...quizQuestion.incorrect_answers,
-          quizQuestion.correct_answer,
-        ];
-        const shuffledAnswers = shuffle(newAnswers);
-        return {
-          id: nanoid(),
-          ...quizQuestion,
-          incorrect_answers: shuffledAnswers,
-          selected_answer: "",
-        };
-      });
-      setQuizData(newArray);
-    }
-    apiData();
+
+    getNewQuiz();
   }, []);
 
   return (
@@ -91,6 +100,9 @@ function App() {
           quizData={quizData}
           insertSelectedAnswer={insertSelectedAnswer}
           checkAnswers={checkAnswers}
+          gameOver={gameOver}
+          newGame={newGame}
+          correctAnswerCount={correctAnswerCount}
         />
       )}
     </main>
--- src/QuizPage.jsx
+++ src/QuizPage.jsx
@@ -1,45 +1,63 @@
 import React from "react";
 
-function QuizPage({ quizData, insertSelectedAnswer, checkAnswers }) {
+function QuizPage({
+  quizData,
+  insertSelectedAnswer,
+  checkAnswers,
+  gameOver,
+  newGame,
+  correctAnswerCount,
+}) {
   let quizElements = quizData.map((quiz) => {
     return (
       <div key={quiz.id} className="question-container">
         <h3>{quiz.question}</h3>
 
         <div className="answers-container">
-          {quiz.incorrect_answers.map((answer, index) => (
-            <div className="button" key={index}>
-              <input
-                name={quiz.id}
-                type="radio"
-                id={`option-${quiz.id}-${index}`}
-                value={answer}
-                checked={quiz.selected_answer === answer}
-                onChange={(event) => {
-                  insertSelectedAnswer(event, quiz.id);
-                }}
-              />
-              <label className="label" htmlFor={`option-${quiz.id}-${index}`}>
-                {answer}
-              </label>
-            </div>
-          ))}
+          {quiz.incorrect_answers.map((answer, index) => {
+            const answerStyle =
+              gameOver && answer === quiz.correct_answer
+                ? { backgroundColor: "#4caf50", borderColor: "#4caf50" }
+                : {};
+            return (
+              <div className="button" key={index}>
+                <input
+                  name={quiz.id}
+                  type="radio"
+                  id={`option-${quiz.id}-${index}`}
+                  value={answer}
+                  checked={quiz.selected_answer === answer}
+                  onChange={(event) => {
+                    insertSelectedAnswer(event, quiz.id);
+                  }}
+                  disabled={gameOver}
+                />
+                <label
+                  className="label"
+                  style={answerStyle}
+                  htmlFor={`option-${quiz.id}-${index}`}>
+                  {answer}
+                </label>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
   });
 
+  const postGameText = <h2>You got {correctAnswerCount}/5 answers right!</h2>;
+
   return (
     <div className="quiz-container">
+      <h1>Trivia Quiz Game</h1>
       {quizElements}
-      <button onClick={() => checkAnswers()}> Check Answer </button>
+      {gameOver && postGameText}
+      <button onClick={gameOver ? newGame : checkAnswers}>
+        {gameOver ? " Play Again " : " Check Answers "}
+      </button>
     </div>
   );
 }
 
 export default QuizPage;
-
-// Get a way to track the selected ID, then send the ID on click to a function which will check the chosen answers against the correct answer. Do for 1 object first then for all.
-// Need a complete game state.
-
-//need to set ID in object for name property in radio buttons
